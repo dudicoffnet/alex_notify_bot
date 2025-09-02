a807a50e0d977790b3e67866c52d931298419072
dudicoffnet <168195277+dudicoffnet@users.noreply.github.com>
2025-09-02 13:10:05 +0300

    Add files via upload

--- handlers/report.py
+++ handlers/report.py
@@ -3,10 +3,10 @@ from aiogram.types import Message
 
 router = Router()
 
-@router.message(commands=["start"])
+@router.message(Text(equals="/start"))
 async def cmd_start(message: Message):
     await message.answer("Бот запущен. Используй /report или /zip")
 
-@router.message(commands=["report"])
+@router.message(Text(equals="/report"))
 async def cmd_report(message: Message):
     await message.answer("📄 Пока что здесь будет твой PDF-отчёт. В разработке.")
